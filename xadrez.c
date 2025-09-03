cffe27108a0c145be21b8477dea2e240812c640b
programacaolauro <robsonlauromeal@gmail.com>
2025-09-02 21:09:30 -0300

    Update xadrez.c

--- xadrez.c
+++ xadrez.c
@@ -7,6 +7,7 @@ int main() {
     int movTorre = 5;
     int movBispo = 5;
     int movRainha = 8;
+    int movCavalo = 2;
 
     int i;
     
@@ -37,5 +38,36 @@ int main() {
         i++;
     } while (i <= movRainha);
 
+        // Movimento do Cavalo
+    
+    printf("\nMovimento do Cavalo:\n");
+
+    int movBaixo = 2;     // número movimentos para baixo
+    int movEsquerda = 1;  // número movimentos para esquerda
+
+    // Loop externo com FOR para organizar os dois estágios do movimento
+
+    int etapa;
+
+    for (int etapa = 0; etapa < 2; etapa++) {
+        if (etapa == 0) {
+
+            // parte 1 - duas casas para baixo
+
+            int cont = 0;
+            while (cont < movBaixo) { // loop interno (while)
+                printf("Baixo\n");
+                cont++;
+            }
+        } else {
+
+            // parte 2 - uma casa para esquerda
+            int cont = 0;
+            while (cont < movEsquerda) { // loop interno (while)
+                printf("Esquerda\n");
+                cont++;
+            }
+        }
+    }
     return 0;
 }
